618fe274eac8db06310008c6d57e63dc2383f064
Ahn Gunnwook <agw76638@protonmail.com>
2025-03-24 17:57:09 +0900

    initial commit

---
+++ src/data/info.ts
@@ -0,0 +1,51 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Kevin Ahn",
+  jobDescription: "Web Developer",
+  about: `I'm a web developer based in South Korea.
+   I enjoy creating stuff.
+   I specialize in HTML, CSS, JavaScript, and I’m exploring the JamStack to expand my skill set.
+  `,
+
+  experience: [
+    {
+      name: "Fiverr",
+      location: "Remote",
+      startDate: "Mar 2025",
+      endDate: "Present",
+      description: [
+        "- Converting design mockups into responsive web pages."]
+        
+    },
+  ],
+
+  education: [
+    {
+      name: "National Korea Maritime & Ocean University",
+      location: "Busan, South Korea",
+      startDate: "2021",
+      endDate: "2025",
+      description: [
+        "Maritime Automous Surface Ship Major",
+      ],
+    }
+    
+  ],
+
+  socialMedia: {
+    //twitter: "https://twitter.com/Uzzii_21",
+    github: "https://github.com/agw76638",
+    email: "agw76638@protonmail.com",
+    linkedin: "https://www.linkedin.com/in/gunnwook-ahn-5401bb355/",
+  },
+
+  projects: [
+    {
+      title: "APOD app",
+      isFeatured: true,
+      thumbnail: "/assets/images/apod.png",
+      githubUrl: "https://github.com/agw76638/apod-app",
+      liveUrl: "https://main--classy-donut-e4ee4c.netlify.app/",
+    },
+  ],
+};
